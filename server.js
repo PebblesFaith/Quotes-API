51fd33ab002825fecc66479e9f0b04f9593e6d87
Sarai H Ajai <ajai.sarai.h@outlook.com>
2019-08-27 16:21:41 -0500

    Update server.js

--- server.js
+++ server.js
@@ -23,3 +23,45 @@ quotesApp.listen(port, function() {
     console.log('Express is listening on port ' + port);
 });
 
+
+const quotes = [
+    {
+        id: 1,
+        quote: "The best is yet to come",
+        author: "Unknown",
+        year: 2000
+    },
+    {
+        id: 2,
+        quote: "This is a quote",
+        author: "First Last",
+        year: 1930
+    },
+    {
+        id: 3,
+        quote: "This is another quote",
+        author: "First2 Last2",
+        year: 1910
+    },
+];
+
+// Returns a list of quotes as JSON.
+quotesApp.get('/quotes', function(request, response){
+    console.log('Get a list of all quotes as JSON.');
+//    response.json(quotes);
+
+
+if(request.query.year) {
+    response.send('Return a list of quotes from the year: ' + request.query.year);
+}
+else {
+    response.json(quotes);
+}
+
+}); 
+
+quotesApp.get('/quotes/:id', function(request, response){
+    console.log('Return quote with the ID: ' + request.params.id);
+    response.send('Return quote with the ID: ' + request.params.id);
+});
+
